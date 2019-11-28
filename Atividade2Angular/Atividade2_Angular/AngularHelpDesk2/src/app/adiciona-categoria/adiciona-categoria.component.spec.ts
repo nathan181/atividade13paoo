c357ae73a315a7dbb801faee7d50ee0aa439b07d
nathan181 <53912862+nathan181@users.noreply.github.com>
2019-11-27 21:47:26 -0300

    Update adiciona-categoria.component.spec.ts

--- Atividade2Angular/Atividade2_Angular/AngularHelpDesk2/src/app/adiciona-categoria/adiciona-categoria.component.spec.ts
+++ Atividade2Angular/Atividade2_Angular/AngularHelpDesk2/src/app/adiciona-categoria/adiciona-categoria.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AdicionaCategoriaComponent } from './adiciona-categoria.component';
+import { AdicionaCategoriaComponent } from './adicionacategoria.component';
 
 describe('AdicionaCategoriaComponent', () => {
   let component: AdicionaCategoriaComponent;
